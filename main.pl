06aca070dad227f95f602f88b11fa2a8a64b2b77
LAB CCET <labccet@lab-mac-12.local>
2019-03-21 21:46:59 -0300

    Adding basic class/inheritance example

---
+++ main.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+ 
+use MyModule;
+ 
+my $myObj = MyModule->new;
+$myObj->say_hi;
+$myObj->say_hello;
---
+++ myParent.pm
@@ -0,0 +1,16 @@
+package MyParent;
+use strict;
+use warnings;
+ 
+sub new {
+    my ($class) = @_;
+    return bless {}, $class;
+}
+ 
+sub say_hi {
+    my ($self) = @_;
+    print "Hi from MyParent\n";
+    return;
+}
+ 
+1;
